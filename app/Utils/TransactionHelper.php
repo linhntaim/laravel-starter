5b242ac7043b6414c558491bf96a0608ec9998d1
Nguyen Tuan Linh <tuanlinh.nguyen@dsquare.com.vn>
2020-09-23 14:05:46 +0700

    config transaction

--- app/Utils/TransactionHelper.php
+++ app/Utils/TransactionHelper.php
@@ -8,16 +8,32 @@ use Throwable;
 
 class TransactionHelper
 {
+    const TRANSACTION_SCOPE_GLOBAL = 'GLOBAL';
+    const TRANSACTION_SCOPE_SESSION = 'SESSION';
+
     const ISOLATION_LEVEL_REPEATABLE_READ = 'REPEATABLE READ';
     const ISOLATION_LEVEL_READ_COMMITTED = 'READ COMMITTED';
     const ISOLATION_LEVEL_READ_UNCOMMITTED = 'READ UNCOMMITTED';
     const ISOLATION_LEVEL_SERIALIZABLE = 'SERIALIZABLE';
+
+    const ACCESS_MODE_READ_WRITE = 'READ WRITE';
+    const ACCESS_MODE_READ_ONLY = 'READ ONLY';
+
+    const TRANSACTION_SCOPES = [
+        TransactionHelper::TRANSACTION_SCOPE_GLOBAL,
+        TransactionHelper::TRANSACTION_SCOPE_SESSION,
+    ];
 
     const ISOLATION_LEVELS = [
         TransactionHelper::ISOLATION_LEVEL_REPEATABLE_READ,
         TransactionHelper::ISOLATION_LEVEL_READ_COMMITTED,
         TransactionHelper::ISOLATION_LEVEL_READ_UNCOMMITTED,
         TransactionHelper::ISOLATION_LEVEL_SERIALIZABLE,
+    ];
+
+    const ACCESS_MODES = [
+        TransactionHelper::ACCESS_MODE_READ_WRITE,
+        TransactionHelper::ACCESS_MODE_READ_ONLY,
     ];
 
     protected static $instance;
@@ -43,19 +59,24 @@ class TransactionHelper
         $this->hasTransaction = false;
     }
 
-    public function start($connection = null, $isolationLevel = null)
+    public function start($connection = null, $isolationLevel = null, $accessMode = null, $transactionScope = null)
     {
         if (empty($connection)) {
             $connection = config('database.default');
         }
         if (!in_array($connection, $this->currentTransactionConnections)) {
             $connector = DB::connection($connection);
+            $connector->beginTransaction();
             if ($connection == 'mysql') {
-                if (in_array($isolationLevel, static::ISOLATION_LEVELS)) {
-                    $connector->getPdo()->exec('SET SESSION TRANSACTION ISOLATION LEVEL ' . $isolationLevel);
+                if (in_array($isolationLevel, static::ISOLATION_LEVELS) || in_array($accessMode, self::ACCESS_MODES)) {
+                    $transactionScope = in_array($transactionScope, static::TRANSACTION_SCOPES) ? $transactionScope . ' ' : '';
+                    $isolationLevel = in_array($isolationLevel, static::ISOLATION_LEVELS) ? ' ISOLATION LEVEL ' . $isolationLevel : '';
+                    $accessMode = in_array($accessMode, static::ACCESS_MODES) ? ' ' . $accessMode : '';
+                    $connector->getPdo()->exec(
+                        sprintf('SET %sTRANSACTION%s%s', $transactionScope, $isolationLevel, $accessMode)
+                    );
                 }
             }
-            $connector->beginTransaction();
             $this->currentTransactionConnections[] = $connection;
             $this->hasTransaction = true;
         }
