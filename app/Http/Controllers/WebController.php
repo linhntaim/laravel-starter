1d2a8031b2bd1e8e83970ff20042e0992f402283
Nguyen Tuan Linh <tuanlinh.nguyen@dsquare.com.vn>
2021-03-02 16:52:26 +0700

    update

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -8,5 +8,26 @@ namespace App\Http\Controllers;
 
 class WebController extends Controller
 {
+    public function __construct()
+    {
+        $this->setValidationThrown(false);
+    }
 
+    protected function redirect($to = null, $status = 302, $headers = [], $secure = null)
+    {
+        $this->transactionComplete();
+        return redirect($to, $status, $headers, $secure);
+    }
+
+    protected function redirectRoute($route, $parameters = [], $status = 302, $headers = [])
+    {
+        $this->transactionComplete();
+        return redirect()->route($route, $parameters, $status, $headers);
+    }
+
+    protected function view($view = null, $data = [], $mergeData = [])
+    {
+        $this->transactionComplete();
+        return view($view, $data, $mergeData);
+    }
 }
