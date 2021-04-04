b253deb35c9432a0269dbd6a0826b4cf260ca6d0
Nguyen Tuan Linh <linhnt.aim@outlook.com>
2021-04-05 00:24:04 +0700

    change scope

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,7 +46,7 @@
                         <div class="p-6">
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="http://git-gbu.japaneast.cloudapp.azure.com/base/laravel-starter" target="_blank" class="underline text-gray-900 dark:text-white">Homepage</a></div>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://github.com/linhntaim/laravel-starter" target="_blank" class="underline text-gray-900 dark:text-white">Homepage</a></div>
                             </div>
 
                             <div class="ml-12">
@@ -59,7 +59,7 @@
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://homepage-gbu.azurewebsites.net/back-end/" target="_blank" class="underline text-gray-900 dark:text-white">Back-end Docs</a></div>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://linhntaim.com" target="_blank" class="underline text-gray-900 dark:text-white">Author</a></div>
                             </div>
 
                             <div class="ml-12">
@@ -72,7 +72,7 @@
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700">
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="http://git-gbu.japaneast.cloudapp.azure.com/" target="_blank" class="underline text-gray-900 dark:text-white">Gitlab</a></div>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://github.com/linhntaim" target="_blank" class="underline text-gray-900 dark:text-white">Author's Github</a></div>
                             </div>
 
                             <div class="ml-12">
@@ -85,7 +85,7 @@
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="http://git-gbu.japaneast.cloudapp.azure.com/base/" target="_blank" class="underline text-gray-900 dark:text-white">Base Projects</a></div>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://github.com/linhntaim?tab=repositories" target="_blank" class="underline text-gray-900 dark:text-white">Author's Repositories</a></div>
                             </div>
 
                             <div class="ml-12">
@@ -98,7 +98,7 @@
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700">
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://www.npmjs.com/settings/dsquare-gbu/packages" target="_blank" class="underline text-gray-900 dark:text-white">Packages on NPM</a></div>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://www.npmjs.com/~linhntaim" target="_blank" class="underline text-gray-900 dark:text-white">Author's Packages on NPM</a></div>
                             </div>
 
                             <div class="ml-12">
@@ -111,7 +111,7 @@
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://hub.docker.com/orgs/gbuatdsquare" target="_blank" class="underline text-gray-900 dark:text-white">Images on Docker</a></div>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://hub.docker.com/u/linhntaim" target="_blank" class="underline text-gray-900 dark:text-white">Author's Images on Docker</a></div>
                             </div>
 
                             <div class="ml-12">
@@ -130,8 +130,8 @@
                                 <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
                             </svg>
 
-                            <a href="https://homepage-gbu.azurewebsites.net/" target="_blank" class="ml-1 underline">
-                                DSquare - GBU
+                            <a href="https://linhntaim.com" target="_blank" class="ml-1 underline">
+                                linhntaim
                             </a>
                         </div>
                     </div>
