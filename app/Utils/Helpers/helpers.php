45c3f093981c3534ad7de72632d9912867d304cc
Nguyen Tuan Linh <tuanlinh.nguyen@dsquare.com.vn>
2021-04-29 16:06:57 +0700

    update

--- app/Utils/Helpers/helpers.php
+++ app/Utils/Helpers/helpers.php
@@ -15,6 +15,39 @@ function printLine($value)
     echo PHP_EOL;
 }
 
+/**
+ * @param string $json
+ * @param bool $safe
+ * @param int $depth
+ * @param int $flags
+ * @return array
+ */
+function jsonDecodeArray(string $json, bool $safe = true, int $depth = 512, int $flags = 0)
+{
+    $array = json_decode($json, true, $depth, $flags);
+
+    return $safe ? (is_array($array) ? $array : []) : $array;
+}
+
+/**
+ * @param string $file
+ * @param bool $safe
+ * @param int $depth
+ * @param int $flags
+ * @return array
+ * @throws
+ */
+function fileJsonDecodeArray(string $file, bool $safe = true, int $depth = 512, int $flags = 0)
+{
+    if (is_file($file)) {
+        return jsonDecodeArray(file_get_contents($file), $safe, $depth, $flags);
+    }
+    if (!$safe) {
+        throw new App\Exceptions\AppException('File does not exist.');
+    }
+    return [];
+}
+
 function got($value, $default = null)
 {
     if (filled($value)) {
